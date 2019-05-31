45ed88d3ff70d81603733bab9ce7d1231b5b0846
Jethava Bhavin <jethava.bhavin@gmail.com>
2019-06-01 00:34:02 +0530

    Shipping and Billing Agreement Issue

--- Block/Checkout/LayoutProcessor.php
+++ Block/Checkout/LayoutProcessor.php
@@ -5,6 +5,7 @@
  */
 namespace Bhavin\GDPR\Block\Checkout;
 
+use Bhavin\GDPR\Model\Source\DisplayArea;
 /**
  * Class LayoutProcessor
  */
@@ -18,47 +19,58 @@ class LayoutProcessor implements \Magento\Checkout\Block\Checkout\LayoutProcesso
 	public function process($jsLayout) {
 		$om = \Magento\Framework\App\ObjectManager::getInstance();
 		$helper = $om->get('Bhavin\GDPR\Helper\Data');
-
+			
 		if ($helper->isEnableOnShipping()) {
-			$shippingAgreementAttributeCode = 'shipping_address_gdpr';
+			 $agreements = $om->create("Magento\CheckoutAgreements\Model\ResourceModel\Agreement\Collection");
+			 $agreements->addFieldToFilter("display_area",DisplayArea::CHECKOUT_SHIPPING);
+	
+			if($agreements->getSize())
+			{
+				$shippingAgreementAttributeCode = 'shipping_address_gdpr';
 
-			$shippingGdprField = [
-				'component' => 'Bhavin_GDPR/js/view/checkout-shipping-agreements',
-				'config' => [
-					'customScope' => 'shippingAddress.custom_attributes',
-				],
-				'dataScope' => 'shippingAddress.custom_attributes' . '.' . $shippingAgreementAttributeCode,
-				'provider' => 'checkoutProvider',
-				'label' => __('Agreements'),
-				'validation' => [
-					'required-entry' => true,
-				],
-			];
+				$shippingGdprField = [
+					'component' => 'Bhavin_GDPR/js/view/checkout-shipping-agreements',
+					'config' => [
+						'customScope' => 'shippingAddress.custom_attributes',
+					],
+					'dataScope' => 'shippingAddress.custom_attributes' . '.' . $shippingAgreementAttributeCode,
+					'provider' => 'checkoutProvider',
+					'label' => __('Agreements'),
+					'validation' => [
+						'required-entry' => true,
+					],
+				];
+			}
 		}
 		if ($helper->isEnableOnBilling()) {
-			$billingAgreementAttributeCode = 'billing_address_gdpr';
+			$agreements = $om->create("Magento\CheckoutAgreements\Model\ResourceModel\Agreement\Collection");
+			$agreements->addFieldToFilter("display_area",DisplayArea::CHECKOUT_BILLING);
+			if($agreements->getSize())
+			{
+				$billingAgreementAttributeCode = 'billing_address_gdpr';
+				$jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children'][$shippingAgreementAttributeCode] = $shippingGdprField;
 
-			$jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children'][$shippingAgreementAttributeCode] = $shippingGdprField;
-
-			$configuration = $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'];
-			foreach ($configuration as $paymentGroup => $groupConfig) {
-				if (isset($groupConfig['component']) AND $groupConfig['component'] === 'Magento_Checkout/js/view/billing-address') {
-					$dataScopePrefix = $groupConfig['dataScopePrefix'];
-					$billingGdprField = [
-						'component' => 'Bhavin_GDPR/js/view/checkout-billing-agreements',
-						'config' => [
-							'customScope' => "{$dataScopePrefix}.custom_attributes",
-						],
-						'dataScope' => "{$dataScopePrefix}.custom_attributes" . '.' . $billingAgreementAttributeCode,
-						'provider' => 'checkoutProvider',
-						'label' => __('Agreements'),
-						'validation' => [
-							'required-entry' => true,
-						],
-					];
-					$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentGroup]['children']['form-fields']['children'][$billingAgreementAttributeCode] = $billingGdprField;
-				}
+				$configuration = $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'];
+				foreach ($configuration as $paymentGroup => $groupConfig) {
+					if (isset($groupConfig['component']) AND $groupConfig['component'] === 'Magento_Checkout/js/view/billing-address') {
+						$dataScopePrefix = $groupConfig['dataScopePrefix'];
+						$billingGdprField = [
+							'component' => 'Bhavin_GDPR/js/view/checkout-billing-agreements',
+							'config' => [
+								'customScope' => "{$dataScopePrefix}.custom_attributes",
+							],
+							'dataScope' => "{$dataScopePrefix}.custom_attributes" . '.' . $billingAgreementAttributeCode,
+							'provider' => 'checkoutProvider',
+							'label' => __('Agreements'),
+							'validation' => [
+								'required-entry' => true,
+							],
+						];
+						$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentGroup]['children']['form-fields']['children'][$billingAgreementAttributeCode] = $billingGdprField;
+					}
+				}	
 			}
+			
 		}
 
 		return $jsLayout;
